32301edda8f1115146b25c3b5cd3b4fefc8903f6
jangsen <dh7895210@naver.com>
2025-03-25 00:47:11 +0900

    Chore: vite config 변경

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,20 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/',
   plugins: [react()],
-})
+  preview: {
+    port: 5173,
+    strictPort: true,
+  },
+  server: {
+    port: 5173,
+    strictPort: true,
+    host: true,
+    watch: {
+      usePolling: true, // 폴링 방식으로 파일 변경 감지
+    },
+  },
+});
